007e33cf3450b6bbbd85296b8f3a2aafb6f95afa
yazi99 <56828636+yazi99@users.noreply.github.com>
2021-08-06 02:11:48 +0530

    add new route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:smart_pos_mobile/pages/home_page.dart';
+import 'package:smart_pos_mobile/pages/add_shop_page.dart';
 import 'package:smart_pos_mobile/pages/login_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginPage.routeName: (_) => LoginPage(),
+  HomePage.routeName: (_) => HomePage(),
+  AddShopPage.routeName: (_) => AddShopPage()
 };
